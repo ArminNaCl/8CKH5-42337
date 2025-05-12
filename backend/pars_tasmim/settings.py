b3549aa9403cf3821e02c2043a19a804dc4eb1ff
Armin <armin.khoshnamak@gmail.com>
2025-05-12 12:10:55 +0330

    feat(docker) dockerise the project

--- backend/pars_tasmim/settings.py
+++ backend/pars_tasmim/settings.py
@@ -93,11 +93,16 @@ WSGI_APPLICATION = 'pars_tasmim.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "mydb",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
